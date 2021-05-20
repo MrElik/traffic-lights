21902ff284b8306f4ebf093db5c28c3b66537e42
Elik <nukhov_eg@mail.ru>
2021-05-20 21:14:35 +0300

    worked app

--- traffic lights/ViewController.swift
+++ traffic lights/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+// MARK: - IB Outlets
+    @IBOutlet weak var colorView: UIStackView!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+   @IBOutlet weak var startButton: UIButton!
+    
+    enum ColorOptions {
+        case red
+        case yellow
+        case green
+        case off
     }
 
+    var colorOption = ColorOptions.off
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redView.alpha = 0.3
+        redView.layer.cornerRadius = redView.frame.height / 2
+        redView.clipsToBounds = true
+        
+        
+        yellowView.alpha = 0.3
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        yellowView.clipsToBounds = true
+        
+        greenView.alpha = 0.3
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+        greenView.clipsToBounds = true
+        
+    }
 
+    @IBAction func tappedStart() {
+        
+        switch colorOption {
+        case .red:
+            startButton.setTitle("Next", for: .normal)
+            
+            greenView.alpha = 0.3
+            yellowView.alpha = 0.3
+            redView.alpha = 1
+            
+            colorOption = .yellow
+        case .yellow:
+            startButton.setTitle("Next", for: .normal)
+           
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            redView.alpha = 0.3
+            
+            colorOption = .green
+        case .green:
+            startButton.setTitle("Next", for: .normal)
+            
+            redView.alpha = 0.3
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            
+            colorOption = .green
+        case .off:
+            colorOption = .red
+        }
+    }
+    
 }
 
